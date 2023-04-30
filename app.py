c4cf3e2b43a58411a89e2ea3b28be8e3c110824a
Abhinash Bhagat <mr@abhinashbhagat.com.np>
2023-04-30 20:10:39 +0545

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import streamlit as st
 from prophet import Prophet
 from datetime import date
